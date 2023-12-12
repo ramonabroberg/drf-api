807b1c039d22904735daad9a888495f063763589
ramonabroberg <>
2023-12-12 21:13:03 +0000

    add likes app, model and serializer

---
+++ likes/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id', 'owner', 'post', 'created_at'
+        ]
