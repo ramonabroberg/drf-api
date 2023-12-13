f2f5d1b1725548abbcda3214ed4bfcb94c4d3bdb
ramonabroberg <>
2023-12-13 19:33:39 +0000

    update client origin settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -62,8 +63,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    '8000-ramonabroberg-drf-api-w34e6m1j3u.us2.codeanyapp.com',
-    'drf-api-rb-b449fd56fdb1.herokuapp.com'
+    os.environ.get('ALLOWED_HOST'),
+    '8000-ramonabroberg-drf-api-w34e6m1j3u.us2.codeanyapp.com'
 ]
 
 # Application definition
@@ -111,9 +112,12 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^([^.]+)', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
